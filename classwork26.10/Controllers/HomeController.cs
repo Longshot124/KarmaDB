197c25911dfb6cd78bfdb4866848695df098c37f
kamra <kamra@LAPTOP-M49P7DNF>
2022-10-27 18:45:02 +0400

    50% done

---
+++ classwork26.10/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using classwork26._10.Models;
+using classworktest.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace classwork26._10.Controllers
+{
+    public class HomeController:Controller
+    {
+        private readonly AppDbContext _appDbContext;
+        public HomeController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _appDbContext.Products.ToList();
+            var categories = _appDbContext.Categories.ToList();
+            var homeViewModel = new HomeViewModel
+            {
+                Categories = categories,
+                Products = products
+            };
+            return View(homeViewModel);
+
+        }
+    }
+}
---
+++ classwork26.10/Models/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using classworktest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace classwork26._10.Models
+{
+    public class HomeViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
